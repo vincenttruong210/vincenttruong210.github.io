20df152a741e95701b8c57690ceb499ee142dcb9
Vincent Truong <vincent.truong.22@cnu.edu>
2023-12-22 11:21:25 -0500

    Added Favicon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -47,7 +47,7 @@ function Footer() {
           </ul>
         </div>
         <div className="copyright">
-          <h3>&copy; {currYear}, Made with ❤️ by Sanajit Jana.</h3>
+          <h3>&copy; {currYear}, Made by Vincent Truong.</h3>
         </div>
         <a
           href="//www.dmca.com/Protection/Status.aspx?ID=4ce89e88-486f-4e19-9159-e412a842118e"
