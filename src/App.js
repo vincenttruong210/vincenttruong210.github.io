b16d80dd920b3507c1224caf3d8db8d411ec637a
Vincent Truong <vincent.truong.22@cnu.edu>
2023-12-22 10:38:46 -0500

    Added Header and Footer to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from "react";
+import "./App.css";
+
+// import Router
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// import Component
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+
+
+// import Pages
+
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <React.StrictMode>
+        <Header />
+        <Footer />
+    </React.StrictMode>
   );
 }
 
